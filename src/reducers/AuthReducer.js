bf163a49fd3095c52a59c47962560b954576a21b
Kenyon Blue <bluekenyfleo@gmail.com>
2019-01-23 10:27:51 -0500

    created immutable state using redux

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -2,10 +2,13 @@ import { EMAIL_CHANGED } from '../actions/types';
 
 const INITIAL_STATE = { email: ''};
 
+//function with 2 arguments 
+//always have a switch statement and default case
+//can never return undefined
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case EMAIL_CHANGED:
-            
+            return { ...state, email: action.payload};
         default:
             return state;
     }
